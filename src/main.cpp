2ba4b34e6dc62757cf1e4be22f9331dacfe2995a
PierrePruvost03 <pierre.pruvost@epitech.eu>
2026-03-09 11:53:12 +0100

    fat(server): basic implementation of server in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <print>
 #include <iostream>
 
+#include <Server.hpp>
 #include <RuleSet.hpp>
 #include <Entity.hpp>
 
@@ -12,9 +13,28 @@ int main() {
     srand(time(0));
     dng::RuleSet::init("tests/ruleset.json");
 
+    dng::Server server;
+    server.add_handler("/hi", [](const httplib::Request&, httplib::Response& res){
+        res.set_content("Hello World!", "text/plain");
+        res.status = 200;
+    });
+    server.add_handler("/health", [](const httplib::Request&, httplib::Response& res){
+        res.set_content("OK", "text/plain");
+        res.status = 200;
+    });
+
+    try {
+        server.start_async();
+    } catch (const std::exception &e) {
+        std::println(stderr, "Failed to start server: {}",  e.what());
+    }
+
     std::string input;
-    while (1) {
-        std::cin >> input;
+    while (std::cin >> input) {
+        if (input == "exit" || input == "quit") {
+            break;
+        }
+
         auto res = dng::RuleSet::entities[0].roll(input);
         if (res)
             std::println("You have rolled {} + {} ({}) in {}.",
@@ -22,5 +42,7 @@ int main() {
         else
             std::println("Error: {}", res.error());
     }
+    server.stop();
+    server.join();
     return 0;
 }
