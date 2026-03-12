2b62c3a0d7fd925a118c81cae091e1814bdbb09f
PierrePruvost03 <pierre.pruvost@epitech.eu>
2026-03-12 11:18:02 +0100

    fix(main): default main with server only (will be engine when it'll be up

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,15 @@
  * Copyright 2026 <D&Gine Group>
  */
 
-#include <print>
-#include <iostream>
-
-#include <Server.hpp>
-#include <RuleSet.hpp>
-#include <Entity.hpp>
+#include "Server.hpp"
 
 int main() {
-    srand(time(0));
-    dng::RuleSet::init("tests/ruleset.json");
-
     dng::Server server;
 
-    try {
-        server.start_async();
-    } catch (const std::exception &e) {
-        std::println(stderr, "Failed to start server: {}",  e.what());
-    }
+    server.add_get_handler("/health", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content(R"({"status":"ok"})", "application/json");
+    });
 
-    std::string input;
-    while (std::cin >> input) {
-        if (input == "exit" || input == "quit") {
-            break;
-        }
-
-        auto res = dng::RuleSet::entities[0].roll(input);
-        if (res)
-            std::println("You have rolled {} + {} ({}) in {}.",
-                (*res).first, (*res).second, (*res).first + (*res).second, input);
-        else
-            std::println("Error: {}", res.error());
-    }
-    server.stop();
-    server.join();
+    server.start();
+    return 0;
 }
